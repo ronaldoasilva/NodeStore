0efc3963c3d277c48570002dca63f9e3b12a1675
Ronaldo Araújo <ronaldo.as@gmail.com>
2017-11-21 14:28:41 -0200

    config
    config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'fsb99';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo a node store';
 
 module.exports = {
-    connectionString: 'mongodb://testenode:testenode@ds036079.mlab.com:36079/testenode',
+    connectionString: 'mongo',
     sendgridKey: 'CriarContaNoSendGridEColocarOSendGridKeyAqui',
     containerConnectionString: 'UsadoConnectionStringStoregeDoAzure'
 }
